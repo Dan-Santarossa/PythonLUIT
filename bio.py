58d65439b64f3f50a75d095b0c4e0e9720bc8955
EC2 Default User <ec2-user@ip-172-31-5-144.ec2.internal>
2022-06-21 22:43:28 +0000

    changed the print function using values

--- bio.py
+++ bio.py
@@ -2,7 +2,8 @@ name = input("What is your name? ")
 colour = input("What is your favourite colour? ")
 age = int(input("How old are you? "))
 
-print(name, end=" ")
-print("is " + str(age) + " years old", end=" ")
-print("and loves the colour " + colour + ".", end=" ")
+#print(name, end=" ")
+#print("is " + str(age) + " years old", end=" ")
+#print("and loves the colour " + colour + ".", end=" ")
 
+print(name, 'is', age, 'years old and loves the colour', colour, ".", sep=" ")
