708234e26a5e6f088afe3c7d1b441257e3abe889
EC2 Default User <ec2-user@ip-172-31-5-144.ec2.internal>
2022-06-21 21:56:13 +0000

    added print strings

--- bio.py
+++ bio.py
@@ -1,3 +1,8 @@
 name = input("What is your name? ")
 colour = input("What is your favourite colour? ")
 age = int(input("How old are you? "))
+
+print(name)
+print("is " + str(age) + " years old")
+print("and loves the colour " + colour + ".")
+
