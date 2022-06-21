a57fdcfef91bf8ee944b0d0e2f40adfb7c0f0533
EC2 Default User <ec2-user@ip-172-31-5-144.ec2.internal>
2022-06-21 22:38:02 +0000

    removed default print newlines

--- bio.py
+++ bio.py
@@ -2,7 +2,7 @@ name = input("What is your name? ")
 colour = input("What is your favourite colour? ")
 age = int(input("How old are you? "))
 
-print(name)
-print("is " + str(age) + " years old")
-print("and loves the colour " + colour + ".")
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the colour " + colour + ".", end=" ")
 
