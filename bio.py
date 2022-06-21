4f65aea5adada50825bf321c7fbfd905e48e0dc2
EC2 Default User <ec2-user@ip-172-31-5-144.ec2.internal>
2022-06-21 22:53:28 +0000

    corrected separator at end of sentence

--- bio.py
+++ bio.py
@@ -6,4 +6,4 @@ age = int(input("How old are you? "))
 #print("is " + str(age) + " years old", end=" ")
 #print("and loves the colour " + colour + ".", end=" ")
 
-print(name, 'is', age, 'years old and loves the colour', colour, ".", sep=" ")
+print(name, 'is', age, 'years old and loves the colour', colour + '.')
