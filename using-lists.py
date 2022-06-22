a72ee6a45f1aba93378ce351ba4759ffd0fba304
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-22 21:19:47 +0000

    finalized lab

--- using-lists.py
+++ using-lists.py
@@ -26,9 +26,11 @@ users.insert(1, 'melody')
 assert users == ['kevin', 'melody', 'alice'], f"Expected `users` to be ['kevin', 'melody', 'alice'] but got: {repr(users)}"
 
 # 6) Add the users 'andy', 'wanda', and 'jim' to the users list using a single command
+users += ['andy', 'wanda', 'jim']
 
 assert users == ['kevin', 'melody', 'alice', 'andy', 'wanda', 'jim'], f"Expected `users` to be ['kevin', 'melody', 'alice', 'andy', 'wanda', 'jim'] but got: {repr(users)}"
 
 # 7) Slice the users lists to return the 3rd and 4th items and assign the result to `center_users`
+center_users = users[2:4]
 
 assert center_users == ['alice', 'andy'], f"Expected `users` to be ['alice', 'andy'] but got: {repr(center_users)}"
