38d949198c8bae5912677f1bf27ebbba31fc6633
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-22 21:08:48 +0000

    deleted user bob

--- using-lists.py
+++ using-lists.py
@@ -4,10 +4,14 @@ users = []
 assert users == [], f"Expected `users` to be [] but got: {repr(users)}"
 
 # 2) Add 'kevin', 'bob', and 'alice' to the users list in that order without reassigning the variable.
+users.append('kevin')
+users.append('bob')
+users.append('alice')
 
 assert users == ['kevin', 'bob', 'alice'], f"Expected `users` to be ['kevin', 'bob', 'alice'] but got: {repr(users)}"
 
 # 3) Remove 'bob' from the users list without reassigning the variable.
+del users[1]
 
 assert users == ['kevin', 'alice'], f"Expected `users` to be ['kevin', 'alice'] but got: {repr(users)}"
 
