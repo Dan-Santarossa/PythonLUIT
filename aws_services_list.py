205c2e35f2f279c980cea4a645530cf9d61174cd
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-23 02:39:55 +0000

    starting list project

---
+++ aws_services_list.py
@@ -0,0 +1,9 @@
+#Create a list of AWS services using Python
+#The list should be empty initially
+#Populate the list using append or insert
+#Print the list and the length of the list
+#Remove two specific services from the list by name or by index
+#Print the new list and the new length of the list
+
+#empty list
+aws_services = []
