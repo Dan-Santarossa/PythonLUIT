5cadddbca0fa57e441f6adb2b234578354df49f9
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-25 19:12:01 -0400

    assert items

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+bblevels = {}
+
+assert bblevels == {
+    '1st Floor Sickroom': 1, 'Hunters Dream': 2, 'Central Yharnam': 3, 'Great Bridge': 4, 'Tomb of Oedon': 5, 'Cathedral Ward': 5, 
+    'Old Yharnam': 6, 'Healing Church Workshop': 7, 'Cathedral Ward revisited': 8, 'Hemwick Charnel Lane': 9, 
+    'Hypogean Gaol': 10, 'Lecture Building 1st Floor': 11, 'Nightmare Frontier': 12, 'Forbidden Woods': 13, 
+    'Iosefkas Clinic': 14, 'Castle Cainhurst': 15, 'Byrgenwerth': 16, 'Yahargul Unseen Village': 17, 'Upper Cathedral Ward': 18, 
+    'Orphanage': 19, 'Lecture Building 2nd Floor': 20, 'Nightmare of Mensis': 21, 'The Old Hunters DLC': 22, 'Hunters Nightmare DLC': 23, 'Research Hall DLC': 24, 'Fishing Hamlet DLC': 25
+}
+
+print (bblevels)
+
