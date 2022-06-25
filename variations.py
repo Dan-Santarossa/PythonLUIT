774a7f6fbf6c8e63860d85783976a04dbd46bd34
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-25 02:43:58 +0000

    adding print methods

---
+++ variations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+
+#Python implementaion here
+
+message = input("Enter a message: ")
+
+print("Lowercase:", message.lower())
+print("Uppercase:", message.upper())
+print("Capitalized:", message.capitalize())
+print("Title Case:", message.title())
