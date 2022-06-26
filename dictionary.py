18ac0bb3701a4d64ae288d27736320eef101afed
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-26 04:07:27 +0000

    dictionary and empty list

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,15 @@
-bblevels = {}
+dict = {1:"Firstst Floor Sickroom", 2: "Hunters Dream", 3: "Central Yharnam", 4: "Great Bridge", 5: "Tomb of Oedon", 
+6: "Cathedral Ward", 7: "Old Yharnam", 8: "Healing Church Workshop", 8: "Cathedral Ward revisited", 9: "Hemwick Charnel Lane", 
+10: "Hypogean Gaol", 11: "Lecture Building Firstst Floor", 12: "Nightmare Frontier", 13: "Forbidden Woods", 14: "Iosefkas Clinic", 
+15: "Castle Cainhurst", 16: "Byrgenwerth", 17: "Yahargul Unseen Village", 18: "Upper Cathedral Ward", 19: "Orphanage", 
+20: "Lecture Building Secondnd Floor", 21: "Nightmare of Mensis", 
+22: "The Old Hunters DLC", 23: "Hunters Nightmare DLC", 24: "Research Hall DLC", 25: "Fishing Hamlet DLC" }
 
-assert bblevels == {
-    '1st Floor Sickroom': 1, 'Hunters Dream': 2, 'Central Yharnam': 3, 'Great Bridge': 4, 'Tomb of Oedon': 5, 'Cathedral Ward': 5, 
-    'Old Yharnam': 6, 'Healing Church Workshop': 7, 'Cathedral Ward revisited': 8, 'Hemwick Charnel Lane': 9, 
-    'Hypogean Gaol': 10, 'Lecture Building 1st Floor': 11, 'Nightmare Frontier': 12, 'Forbidden Woods': 13, 
-    'Iosefkas Clinic': 14, 'Castle Cainhurst': 15, 'Byrgenwerth': 16, 'Yahargul Unseen Village': 17, 'Upper Cathedral Ward': 18, 
-    'Orphanage': 19, 'Lecture Building 2nd Floor': 20, 'Nightmare of Mensis': 21, 'The Old Hunters DLC': 22, 'Hunters Nightmare DLC': 23, 'Research Hall DLC': 24, 'Fishing Hamlet DLC': 25
-}
+#print (Dict)
 
-print (bblevels)
+print (dict)
 
+list = ()
+
+print ("This is an empty list") 
+print (list)
