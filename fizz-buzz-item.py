38cc5fdad242a93511cbdab0bd6fd909e195c688
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-27 21:20:45 +0000

    added elifs division statments

--- fizz-buzz-item.py
+++ fizz-buzz-item.py
@@ -3,3 +3,12 @@
 #Python implementation goes here
 value = int(input("Enter and interger value: "))
 
+if value % 5 == 0  and value % 3 == 0:
+    print("FizzBuzz")
+elif value % 3 == 0:
+    print ("Fizz")
+elif value % 5 == 0:
+    print ("Buzz")
+else:
+    print(value)
+
