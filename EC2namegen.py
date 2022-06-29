f5ecc52722855bdc7ddd8b5bdcd64c3c5ea15138
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-29 21:15:14 +0000

    if statement to exit generator if wrong dept

---
+++ EC2namegen.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.7
+
+import random
+
+department = input("What is your Department? (marketing, accounting, finops) : ")
+    
+if (department != 'marketing' and 'accounting' and 'finops'):
+    print ("You are not authorized to use this EC2 name generator.")
+    
