ca65bf6346b106a74865b88a97602533b8c15c02
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-29 21:29:51 +0000

    additional 'trying to cont. loop if dept is correct

--- EC2namegen.py
+++ EC2namegen.py
@@ -4,6 +4,8 @@ import random
 
 department = input("What is your Department? (marketing, accounting, finops): ")
     
-if (department != 'marketing' and != 'accounting' and != 'finops'):
+if (department != 'marketing' and department != 'accounting' and department != 'finops'):
     print ("You are not authorized to use this EC2 name generator.")
-    
+
+    elif(department == 'marketing' and department == 'accounting' and department == 'finops'):
+    print ("Please enter a number of EC2 instances" )
