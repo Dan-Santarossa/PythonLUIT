fe93311151fb64b35fa785f1b3eb7e957639b071
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-29 17:07:37 +0000

    check if iteration is divisble by 3 and 5

--- fizz-buzz-loops.py
+++ fizz-buzz-loops.py
@@ -3,4 +3,7 @@
 upper_number = int(input("How many values should we process?: "))
 
 for number in range(1, upper_number + 1):
-    print(number)
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(number)
