d99647c06dd7613636770bc700c30e77acaf7d9c
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-29 21:21:15 +0000

    additional 'not equal to' in if statement

--- EC2namegen.py
+++ EC2namegen.py
@@ -4,6 +4,6 @@ import random
 
 department = input("What is your Department? (marketing, accounting, finops): ")
     
-if (department != 'marketing' and 'accounting' and 'finops'):
+if (department != 'marketing' and != 'accounting' and != 'finops'):
     print ("You are not authorized to use this EC2 name generator.")
     
