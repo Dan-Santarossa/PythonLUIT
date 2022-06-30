8f0b27ea413629a4e0bb1fd6c6e1e041db512ca3
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-30 19:11:52 +0000

    generator excerise fib addition

--- generator.py
+++ generator.py
@@ -3,3 +3,9 @@ def gen_range(stop, start=1, step=1):
     while num <= stop:
         yield num
         num += step
+
+def gen_fib():
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        yield a
