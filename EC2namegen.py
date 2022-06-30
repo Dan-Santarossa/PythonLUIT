3e3d19108957266fe624019eeadca29ef4105cfd
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-30 23:01:24 +0000

    trying to create generator step

--- EC2namegen.py
+++ EC2namegen.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3.7
 import random
 
+#Welcome to program
 print("Welcome to the EC2 unique name generator")
 print("")
 
+#Department check
 department = input("Please state your department (marketing, accounting, finops): ")
 
 if (department != 'marketing' and department != 'accounting' and department != 'finops'):
     print ("You are not authorized to use this EC2 name generator. ")
 else: 
-    print (input("Please enter a number of EC2 instances: "))
-        
-        
-        
-        
-        
+    amount = int(input("Enter the amount of EC2 instances: "))
 
-
-
-#amount = int(input("Enter the amount of EC2 instances: "))
-#if (department == 'marketing' and department == 'accounting' and department == 'finops'):
+def generator(department, amount):
+    names = []
+    department = department.lower
+    
