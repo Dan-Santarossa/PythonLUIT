2d97ce674d6204b4998b56a5a63319fd94364226
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-30 17:23:12 +0000

    fixed if/else loop

--- EC2namegen.py
+++ EC2namegen.py
@@ -3,14 +3,15 @@
 import random
 
 
-department = input("Please state your Department (marketing, accounting, finops): ")
+department = input("Please state your department (marketing, accounting, finops): ")
 #amount = int(input("Enter the amount of EC2 instances: "))
 
-for dept in department:    
-    if (department != 'marketing' and department != 'accounting' and department != 'finops'):
-        print ("You are not authorized to use this EC2 name generator. ")
-        exit()
+#for dept in department:    
+if (department != 'marketing' and department != 'accounting' and department != 'finops'):
+    print ("You are not authorized to use this EC2 name generator. ")
     
-    elif (department == 'marketing' and department == 'accounting' and department == 'finops'):
-            print (input("Please enter a number of EC2 instances: "))
+
+else: 
+    #if (department == 'marketing' and department == 'accounting' and department == 'finops'):
+    print (input("Please enter a number of EC2 instances: "))
         
