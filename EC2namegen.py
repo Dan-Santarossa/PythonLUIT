92296117de85aa0a7c4e0648df1e832b83ba406f
Dan-Santarossa <dpsantarossa@gmail.com>
2022-06-30 17:14:43 +0000

    still working on for loop

--- EC2namegen.py
+++ EC2namegen.py
@@ -4,13 +4,13 @@ import random
 
 
 department = input("Please state your Department (marketing, accounting, finops): ")
-amount = int(input("Enter the amount of EC2 instances:" ))
+#amount = int(input("Enter the amount of EC2 instances: "))
 
 for dept in department:    
     if (department != 'marketing' and department != 'accounting' and department != 'finops'):
-        print ("You are not authorized to use this EC2 name generator.")
+        print ("You are not authorized to use this EC2 name generator. ")
         exit()
     
     elif (department == 'marketing' and department == 'accounting' and department == 'finops'):
-        print (input("Please enter a number of EC2 instances. "))
+            print (input("Please enter a number of EC2 instances: "))
         
