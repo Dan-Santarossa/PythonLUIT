7c6e43fd4a711cc1115aa89dd80182852368b5c5
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-02 22:23:02 +0000

    fixed print output that was contained in my for loop

--- ec2namegen_nofunc.py
+++ ec2namegen_nofunc.py
@@ -12,22 +12,23 @@ depts = ['marketing', 'accounting', 'finops']
 #mininstances = 1
 ec2list = []
 
-#Department check
+#Department input and check
 department = input("Please state your department (Marketing, Accounting, FinOps): ").lower()
-
+#If check department unsuccessful print unauthorized message
 if (department not in depts):
     print ("You are not authorized to use this EC2 name generator. ")
-else: 
+else: #if department check successful ask for amount of names
     print("")
     amount = int(input("Enter the amount of EC2 instances: "))
-    
+    #instance name creation. created instance, randomnum, randomchar, ec2name variables
     for instance in range(0, amount):
         randomnum = random.randint(100, 999)
         randomchar = ''.join(random.choices(string.ascii_letters, k=5))
         ec2name = "{}_{}{}{}".format(department, randomnum, randomchar, instance)
         ec2list.append(ec2name)
-        
-        print("\n".join(ec2list))
+        #print formatted list
+    
+    print("\n".join(ec2list))
         
         
         
