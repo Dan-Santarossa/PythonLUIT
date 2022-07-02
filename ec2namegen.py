b7f494a58ec9fab102db87b8f8ff2d72761f0cad
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-02 21:03:33 +0000

    completed function, added notes

--- ec2namegen.py
+++ ec2namegen.py
@@ -10,15 +10,15 @@ def generator(department, amount):
     #department check
     if (department not in depts):
         print ("You are not authorized to use this EC2 name generator. ")
-    else: #instance name creation
-       
+    else: 
+        #instance name creation
         for instance in range(0, amount):
             randomnum = random.randint(100, 999)
             randomchar = ''.join(random.choices(string.ascii_letters, k=5))
             ec2name = "{}_{}{}{}".format(department, randomnum, randomchar, instance)
             ec2list.append(ec2name)
-    
-        print("\n".join(ec2list)) #print list
+        #print list
+        print("\n".join(ec2list))
             
     return ec2list
     
