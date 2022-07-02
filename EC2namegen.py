7e8d0ebf076658c363f16829f2dadeedcb0bb9b0
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-02 03:00:42 +0000

    working on project

--- EC2namegen.py
+++ EC2namegen.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3.7
 import random
+import string
 
 #Welcome to program
 print("Welcome to the EC2 unique name generator")
 print("")
 
-depts = ['Marketing', 'Accounting', 'Finops']
+depts = ['marketing', 'accounting', 'finops']
 maxinstances = 1000
 mininstances = 1
+ec2list = []
 
 #Department check
-department = input("Please state your department (marketing, accounting, finops): ")
+department = input("Please state your department (Marketing, Accounting, FinOps): ").lower()
 
 if (department not in depts):
     print ("You are not authorized to use this EC2 name generator. ")
@@ -21,9 +23,40 @@ else:
     elif amount < mininstances:
         print ("Amount of instances to low {}".format(mininstances))
     else:
-        random.randint(0, 999)
+        
+        for instance in range(0, amount):
+            randomnum = random.randint(0, 999)
+            randomchar = ''.join(random.choices(string.ascii_letters, k=5))
+            ec2name = "{}_{}{}{}".format(department, randomnum, randomchar, instance)
+            ec2list.append(ec2name)
 
-#def generator(department, amount):
- #    = []
-  #  department = department.lower
+        print("\n".join(ec2list))
+        
+
+def generator(department, amount):
+    depts = ['marketing', 'accounting', 'finops']
+    maxinstances = 1000
+    mininstances = 1
+    ec2list = []
+    
+    #Department check
+    department = input("Please state your department (Marketing, Accounting, FinOps): ").lower()
+    
+    if (department not in depts):
+        print ("You are not authorized to use this EC2 name generator. ")
+    else: 
+        amount = int(input("Enter the amount of EC2 instances: "))
+        if amount > maxinstances:
+            print ("Amount of instances to high {}".format(maxinstances))
+        elif amount < mininstances:
+            print ("Amount of instances to low {}".format(mininstances))
+        else:
+            
+            for instance in range(0, amount):
+                randomnum = random.randint(0, 999)
+                randomchar = ''.join(random.choices(string.ascii_letters, k=5))
+                ec2name = "{}_{}{}{}".format(department, randomnum, randomchar, instance)
+                ec2list.append(ec2name)
+    
+            print("\n".join(ec2list))
     
