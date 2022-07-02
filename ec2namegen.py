41093cdd52a3e157e50547ccb9ac40a428ba7b6d
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-02 06:14:18 +0000

    changed stupid capitals in file name

---
+++ ec2namegen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.7
+import random
+import string
+
+#Welcome to program
+def generator(department, amount):
+    depts = ['marketing', 'accounting', 'finops']
+    ec2list = []
+    
+    department = department.lower()
+    if (department not in depts):
+        print ("You are not authorized to use this EC2 name generator. ")
+    else: 
+       
+        for instance in range(0, amount):
+            randomnum = random.randint(100, 999)
+            randomchar = ''.join(random.choices(string.ascii_letters, k=5))
+            ec2name = "{}_{}{}{}".format(department, randomnum, randomchar, instance)
+            ec2list.append(ec2name)
+    
+        print("\n".join(ec2list))
+            
+    return ec2list
+    
+#if __name__ == "__main__":
+ #   x = generator()
+  #  print("\n".join(x))
+    
+    
+    
+
