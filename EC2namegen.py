25d1dab49bf0edbb3bd5f7812d59bef9f215f4ff
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-02 01:09:45 +0000

    working on project

--- EC2namegen.py
+++ EC2namegen.py
@@ -5,15 +5,29 @@ import random
 print("Welcome to the EC2 unique name generator")
 print("")
 
+depts = ['Marketing', 'Accounting', 'Finops']
+maxinstances = 1000
+
+x = 'marKeting'
+print(x)
+y = x
+print(y)
+z = x.lower()
+print(z)
+print(x)
+print(depts)
+
 #Department check
 department = input("Please state your department (marketing, accounting, finops): ")
 
-if (department != 'marketing' and department != 'accounting' and department != 'finops'):
+if (department not in depts):
     print ("You are not authorized to use this EC2 name generator. ")
 else: 
     amount = int(input("Enter the amount of EC2 instances: "))
+    if amount > maxinstances:
+        print ("Amount of instances to high ({}{} Max)".format(maxinstances, x))
 
-def generator(department, amount):
-    names = []
-    department = department.lower
+#def generator(department, amount):
+ #    = []
+  #  department = department.lower
     
