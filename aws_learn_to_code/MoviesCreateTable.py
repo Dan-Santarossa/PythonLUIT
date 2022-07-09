07fd092df1aed84a99d3e29d2af5cf1acbf92ce0
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-09 12:32:40 -0400

    changed region

--- aws_learn_to_code/MoviesCreateTable.py
+++ aws_learn_to_code/MoviesCreateTable.py
@@ -1,6 +1,6 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 table = dynamodb.create_table(
     TableName='Movies',
