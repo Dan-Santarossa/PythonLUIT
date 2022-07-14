53561e89ba0213caf55cdf7b8e03782b4ef07678
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-14 15:43:28 -0400

    policy and create sqs updated

--- lambda_project_week_15/sqs-create.py
+++ lambda_project_week_15/sqs-create.py
@@ -8,7 +8,7 @@ import boto3
 sqs = boto3.resource('sqs')
 
 ## Create the queue. This returns an SQS queue. Enter your own desired queue name. 
-queue = sqs.create_queue(QueueName='your-queue-name')
+queue = sqs.create_queue(QueueName='project15queue')
 
 ## Print the queue url for record
 print(queue.url)
