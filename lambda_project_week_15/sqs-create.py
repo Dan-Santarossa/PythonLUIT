04d6d96613e741ca015031b5000abce2997ea114
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-14 14:03:48 -0400

    create sqs python file

---
+++ lambda_project_week_15/sqs-create.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.10
+## Script that creates a Standard SQS Queue
+
+## Import the boto3 client
+import boto3
+
+## Get the service resource for SQS
+sqs = boto3.resource('sqs')
+
+## Create the queue. This returns an SQS queue. Enter your own desired queue name. 
+queue = sqs.create_queue(QueueName='your-queue-name')
+
+## Print the queue url for record
+print(queue.url)
