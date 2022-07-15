87e3c742917d7d92f662078eecd7fcd7907455e9
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-15 19:37:59 -0400

    table for dynamodb section of project week 15

---
+++ lambda_project_week_15/dynamodb_table_create_datetime.py
@@ -0,0 +1,37 @@
+#!usr/bin/env python3.7
+
+import boto3
+
+#Get the service resource.                        #Set appropriate region
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+#Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='DateTime-Table',
+    KeySchema=[
+        {
+            'AttributeName': 'DateTime',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'ID',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'DateTime',
+            'AttributeType': 'S'  #S is for string attribute
+        },
+        {
+            'AttributeName': 'ID',
+            'AttributeType': 'N'  #N is for number attibute
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+print("Table status:", table.table_status) #print table status
