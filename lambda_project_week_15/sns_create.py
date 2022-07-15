2ad2ff860ea6ac5efde35e4934853429a9e77205
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-14 22:58:42 -0400

    spelling error on end point

--- lambda_project_week_15/sns_create.py
+++ lambda_project_week_15/sns_create.py
@@ -1,14 +1,17 @@
 import boto3
 
 sns = boto3.client("sns",
-                  region_name="us-east-1")
+                   region_name="us-east-1")
 
 response = sns.create_topic(Name="Project15_sns")
 topic_arn = response["TopicArn"]
 
 print(topic_arn)
 
-# response = sns.subscribe(TopicArn=topic_arn,
-#                          Protocol="email",
-#                          Endpint="dpsantarossa@gmail.com")
+sub = sns.subscribe(TopicArn=topic_arn,
+                         Protocol="email",
+                         Endpoint="dpsantarossa@gmail.com")
 
+subscription_arn = sub("SubscriptionArn")
+
+print(subscription_arn)
